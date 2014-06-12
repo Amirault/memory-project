8c15c0a0d902a6d06d034e83613b216c6979a13a
amirault.tony@gmail.com <amirault.tony@gmail.com@df041b98-7b34-8920-a65c-837372e7264c>
2014-06-12 11:43:09 +0000

    // correction...

--- client/index.php
+++ client/index.php
@@ -202,33 +202,6 @@
 				</div>
 			</div>
 		</div>
-			<div class="modal-header">
-				<h3>Bienvenue, veuillez vous identifier :</h3>
-			</div>
-			<div class="modal-body">
-				<div class="row-fluid">
-					<div class="span1"></div>
-					<div class="span4">
-							<input id="pseudo" name="pseudo" placeholder="Votre Pseudo" class="input-xlarge"  type="text" required/>
-							<input id="mdp" name="mdp" placeholder="Votre mot de passe" class="input-xlarge"  type="password" required/>
-							<a href="#"  id="activeInscription">S'inscrire</a>
-							ou
-							<a href="#" id="activeConnexion">Se connecter</a>
-							<input id="validMdp" class="hide" name="validMdp" placeholder="Confirmer le mot de passe" class="input-xlarge"  type="password" required/> 
-					</div>
-					<div class="span1"></div>
-				</div>
-			</div>
-			<div class="modal-footer">
-				<div class="row-fluid">
-					<div class="span1"></div>
-					<div class="span10" style="text-align:center">
-						<button  class="btn" id="validForm" value="connexion">Connexion</button>
-					</div>
-					<div class="span1"></div>
-				</div>
-			</div>
-		</div>
 		<div id="modalBeforeGame" class="modal hide fade" >
 			<div class="modal-header">
 				<h3>Avant de jouer :</h3>
